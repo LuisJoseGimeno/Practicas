172e48aaaed743c3e7d510e11d210d9acf5f8bb0
LuisJoseGimeno <luisjosegimeno@gmail.com>
2023-10-11 17:47:06 +0200

    actividad 1

--- app/src/main/java/com/example/practicas/MainActivity.kt
+++ app/src/main/java/com/example/practicas/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.practicas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val next: Button = findViewById(R.id.Next)
+
+        next.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, Activity2::class.java)
+            startActivity(intent)
+        })
     }
 }
